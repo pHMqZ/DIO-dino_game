21b94274a7df4ec035ec6a8872c288b7ac82e92d
Phillip Marques <phillipmarq@hotmail.com>
2022-04-26 12:00:48 -0300

    Dino jumping

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 const dino = document.querySelector('.dino');
 
+let isJumping = false;
+let position = 0;
+
 function handlerKeyUp(event) {
     if (event.keyCode === 32) {
-        console.log('Pressionou espaço');
+        if (!isJumping) {
+            jump();
+        }
     }
+}
+
+function jump() {
+    isJumping = true;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20)
 }
 
 document.addEventListener('keyup', handlerKeyUp);
