5f3db5aa6d523d758afa31fec4c3f735ffc1f54d
Phillip Marques <phillipmarq@hotmail.com>
2022-04-26 12:45:09 -0300

    Implements game system

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 
 let isJumping = false;
+let isGameOver = false;
 let position = 0;
 
 function handlerKeyUp(event) {
@@ -38,11 +39,29 @@ function jump() {
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
 
+    if (isGameOver) return;
 
     cactus.classList.add('cactus');
     background.appendChild(cactus);
     cactus.style.left = 1000 + 'px';
+
+    let leftTimer = setInteral(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftTimer);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            clearInterval(leftTimer);
+            isGameOver = true;
+            document.body.innerHTML = '<h1 class="game-over">Game Over</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
